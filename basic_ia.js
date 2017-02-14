0441bc55386d343815d65f8359149c81ea75febe
Clément François <clement.francois@epitech.eu>
2017-02-14 16:43:47 +0100

    creating object for the treatment

---
+++ basic_ia.js
@@ -0,0 +1,11 @@
+var avg = [{
+    coeff : 10,
+    name : "poid",
+    value : [10.2, 20.4, 30.5, 50.5]
+}, {
+    coeff : 4,
+    name : "température",
+    value : [10.4, 20.8, 40.4, 55.8]
+}]
+
+console.log(avg);
